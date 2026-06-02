4463789ae08d3d067845d78d1317c27fd463a20d
unknown <your@email.com>
2026-06-02 15:40:19 -0500

    Remove logo from auth layout completely

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,10 +8,6 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       <AuthBackdrop />
       <div className="relative z-10 min-h-screen grid place-items-center px-4 py-10">
         <div className="w-full max-w-[460px]">
-          <div className="flex items-center justify-center gap-3 mb-7">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src="/logo2.png" alt="SEOForge" width={48} height={48} className="rounded-xl" />
-          </div>
           {children}
         </div>
       </div>
